3df69631b35e649fa1723752c411953ee01f14e7
Rhode-park <jinah.park.16@gmail.com>
2023-06-10 02:42:39 +0900

    feat: mediListCollectionView 구현

---
+++ Medi Buddy/Medi Buddy/MediListCell.swift
@@ -0,0 +1,12 @@
+//
+//  MediListCell.swift
+//  Medi Buddy
+//
+//  Created by Jinah Park on 2023/06/10.
+//
+
+import UIKit
+
+class MediListCell: UICollectionViewCell {
+    
+}
--- Medi Buddy/Medi Buddy/MediListViewController.swift
+++ Medi Buddy/Medi Buddy/MediListViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 final class MediListViewController: UIViewController {
-
+    private let mediListCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 10
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(MediListCell.self, forCellWithReuseIdentifier: "MediListCell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -17,6 +28,7 @@ final class MediListViewController: UIViewController {
     
     private func configureUI() {
         view.backgroundColor = .white
+        mediListCollectionView.dataSource = self
     }
     
     private func configureNavigationBar() {
@@ -41,5 +53,25 @@ final class MediListViewController: UIViewController {
     private func setMedicineList() {
         print("setMedicineList")
     }
+    
+    private func configureConstraint() {
+        NSLayoutConstraint.activate([
+            mediListCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mediListCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mediListCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mediListCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 }
 
+extension MediListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = mediListCollectionView.dequeueReusableCell(withReuseIdentifier: "MediListCell", for: indexPath) as? MediListCell else { return MediListCell() }
+        
+        return cell
+    }
+}
