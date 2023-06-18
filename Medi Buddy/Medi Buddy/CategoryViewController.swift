279844dd6bf41b3ced3c8150cf86c7c790dd8e78
Rhode-park <jinah.park.16@gmail.com>
2023-06-18 16:59:18 +0900

    feat: 카테고리 선택 기능 구현

--- Medi Buddy/Medi Buddy/CategoryViewController.swift
+++ Medi Buddy/Medi Buddy/CategoryViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 final class CategoryViewController: UIViewController {
+    var categoryList: [Category] {
+        return MedicineManager.shared.categoryList
+    }
+    
     let categoryScrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.showsVerticalScrollIndicator = false
@@ -19,18 +23,19 @@ final class CategoryViewController: UIViewController {
     let categoryStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.backgroundColor = .systemCyan
+        stackView.spacing = 12
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
         return stackView
     }()
     
-    let categoryTitleLabel: UILabel = {
+    let categoryMainTitleLabel: UILabel = {
         let label = UILabel()
         label.text = "카테고리"
         label.font = UIFont.preferredFont(forTextStyle: .subheadline)
         label.textColor = .label
         label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
@@ -40,27 +45,82 @@ final class CategoryViewController: UIViewController {
         view.backgroundColor = .systemBackground
         configureSubView()
         configureConstraint()
+        configureCategoryStackView()
     }
     
     private func configureSubView() {
+        view.addSubview(categoryMainTitleLabel)
         view.addSubview(categoryScrollView)
         categoryScrollView.addSubview(categoryStackView)
-        categoryStackView.addArrangedSubview(categoryTitleLabel)
     }
     
     private func configureConstraint() {
         NSLayoutConstraint.activate([
-            categoryScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 28),
-            categoryScrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 28),
-            categoryScrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -28),
-            categoryScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -28),
+            categoryMainTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            categoryMainTitleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 32),
+            categoryMainTitleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -32),
+            categoryScrollView.topAnchor.constraint(equalTo: categoryMainTitleLabel.bottomAnchor, constant: 16),
+            categoryScrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 32),
+            categoryScrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -32),
+            categoryScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -32),
             categoryStackView.topAnchor.constraint(equalTo: categoryScrollView.contentLayoutGuide.topAnchor),
             categoryStackView.leadingAnchor.constraint(equalTo: categoryScrollView.contentLayoutGuide.leadingAnchor),
             categoryStackView.trailingAnchor.constraint(equalTo: categoryScrollView.contentLayoutGuide.trailingAnchor),
             categoryStackView.bottomAnchor.constraint(equalTo: categoryScrollView.contentLayoutGuide.bottomAnchor),
             categoryStackView.leadingAnchor.constraint(equalTo: categoryScrollView.frameLayoutGuide.leadingAnchor),
             categoryStackView.trailingAnchor.constraint(equalTo: categoryScrollView.frameLayoutGuide.trailingAnchor),
-            categoryTitleLabel.widthAnchor.constraint(equalTo: categoryStackView.widthAnchor),
         ])
+    }
+    
+    private func createCategoryHorizontalStackView(category: Category) {
+        let categoryHorizontalStackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.spacing = 8
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            
+            return stackView
+        }()
+        
+        let categoryButton: UIButton = {
+            let button = UIButton()
+            button.setImage(UIImage(systemName: "checkmark.square.fill"), for: .selected)
+            button.setImage(UIImage(systemName: "square"), for: .normal)
+            button.tintColor = .systemCyan
+            button.setContentHuggingPriority(.required, for: .horizontal)
+            
+            return button
+        }()
+        
+        let categoryTitleLabel: UILabel = {
+            let label = UILabel()
+            label.text = category.name.description
+            label.font = .preferredFont(forTextStyle: .subheadline)
+            label.textColor = .label
+            
+            return label
+        }()
+        
+        categoryStackView.addArrangedSubview(categoryHorizontalStackView)
+        categoryHorizontalStackView.addArrangedSubview(categoryButton)
+        categoryHorizontalStackView.addArrangedSubview(categoryTitleLabel)
+        
+        NSLayoutConstraint.activate([
+            categoryHorizontalStackView.widthAnchor.constraint(equalTo: categoryStackView.widthAnchor),
+        ])
+    }
+    
+    private func configureCategoryStackView() {
+        categoryList.forEach { category in
+            createCategoryHorizontalStackView(category: category)
+        }
+        categoryList.forEach { category in
+            createCategoryHorizontalStackView(category: category)
+        }
+        categoryList.forEach { category in
+            createCategoryHorizontalStackView(category: category)
+        }
+        categoryList.forEach { category in
+            createCategoryHorizontalStackView(category: category)
+        }
     }
 }
