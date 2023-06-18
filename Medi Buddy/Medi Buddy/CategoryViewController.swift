95d86d747c99e0e7813f740d636c0aebb5cb16e8
Rhode-park <jinah.park.16@gmail.com>
2023-06-18 16:31:48 +0900

    feat: CategoryViewController 내부 구현

--- Medi Buddy/Medi Buddy/CategoryViewController.swift
+++ Medi Buddy/Medi Buddy/CategoryViewController.swift
@@ -8,8 +8,59 @@
 import UIKit
 
 final class CategoryViewController: UIViewController {
+    let categoryScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
+    
+    let categoryStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.backgroundColor = .systemCyan
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    let categoryTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "카테고리"
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.textColor = .label
+        label.textAlignment = .left
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureSubView()
+        configureConstraint()
+    }
+    
+    private func configureSubView() {
+        view.addSubview(categoryScrollView)
+        categoryScrollView.addSubview(categoryStackView)
+        categoryStackView.addArrangedSubview(categoryTitleLabel)
+    }
+    
+    private func configureConstraint() {
+        NSLayoutConstraint.activate([
+            categoryScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 28),
+            categoryScrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 28),
+            categoryScrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -28),
+            categoryScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -28),
+            categoryStackView.topAnchor.constraint(equalTo: categoryScrollView.contentLayoutGuide.topAnchor),
+            categoryStackView.leadingAnchor.constraint(equalTo: categoryScrollView.contentLayoutGuide.leadingAnchor),
+            categoryStackView.trailingAnchor.constraint(equalTo: categoryScrollView.contentLayoutGuide.trailingAnchor),
+            categoryStackView.bottomAnchor.constraint(equalTo: categoryScrollView.contentLayoutGuide.bottomAnchor),
+            categoryStackView.leadingAnchor.constraint(equalTo: categoryScrollView.frameLayoutGuide.leadingAnchor),
+            categoryStackView.trailingAnchor.constraint(equalTo: categoryScrollView.frameLayoutGuide.trailingAnchor),
+            categoryTitleLabel.widthAnchor.constraint(equalTo: categoryStackView.widthAnchor),
+        ])
     }
 }
